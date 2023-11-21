1821b6abb84f12a347c4033c2f67af8656f373ab
Phú Đạt <phudat1104@gmail.com>
2023-11-21 09:59:27 +0700

    commit1

---
+++ src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,367 @@
+import React from "react";
+import "./style.css"
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <img className="intersect" alt="Intersect" src="/intersect.sgv" />
+          <div className="ellipse" />
+          <img className="image" alt="Atribute" src="/img/image-11.png" />
+          <img className="mask-group" alt="Mask group" src="/img/mask-group.png" />
+        </div>
+        <div className="overlap-group">
+          <img className="img" alt="Intersect" src="/img/intersect-3.svg" />
+          <div className="card-product-hover">
+            <div className="overlap-2">
+              <img className="img-2" alt="Img" src="/img/img-11.svg" />
+              <p className="titile">Túi Totes Cactus Bag nữ tính xành điệu</p>
+              <div className="overlap-3">
+                <div className="overlap-group-2">
+                  <div className="price">320.000 đ</div>
+                  <img className="cursor-pointer" alt="Cursor pointer" src="/img/cursor-pointer-2.svg" />
+                </div>
+                <div className="color">
+                  <div className="ellipse-2" />
+                  <div className="ellipse-3" />
+                  <div className="ellipse-4" />
+                  <div className="ellipse-5" />
+                </div>
+              </div>
+              <button className="btn">
+                <div className="text-wrapper">Mua ngay</div>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="overlap-4">
+          <img className="rectangle" alt="Rectangle" src="/img/rectangle-3.svg" />
+          <div className="nav-bar">
+            <div className="overlap-group-3">
+              <div className="rectangle-2" />
+              <div className="text-wrapper-2">Trang chủ</div>
+            </div>
+            <div className="overlap-5">
+              <div className="group">
+                <img className="img-3" alt="Bag" src="/img/bag-1.svg" />
+              </div>
+              <div className="ellipse-6" />
+              <div className="text-wrapper-3">2</div>
+            </div>
+            <img className="logo-beeyou" alt="Logo beeyou" src="/img/logo-beeyou.svg" />
+            <div className="text-wrapper-4">Tote collection</div>
+            <div className="text-wrapper-5">Về chúng tôi</div>
+            <div className="text-wrapper-6">Liên hệ</div>
+          </div>
+          <div className="ellipse-7" />
+          <div className="ellipse-8" />
+          <div className="ellipse-9" />
+          <div className="ellipse-10" />
+          <div className="ellipse-11" />
+        </div>
+        <div className="card-product-default">
+          <div className="overlap-6">
+            <img className="img-2" alt="Img" src="/img/img-10.svg" />
+            <p className="titile">Túi Totes Cactus Bag nữ tính xành điệu</p>
+            <div className="price-2">320.000 đ</div>
+            <div className="color-2">
+              <div className="ellipse-2" />
+              <div className="ellipse-3" />
+              <div className="ellipse-4" />
+              <div className="ellipse-5" />
+            </div>
+          </div>
+        </div>
+        <div className="overlap-wrapper">
+          <div className="overlap-7">
+            <img className="img-2" alt="Img" src="/img/img-9.svg" />
+            <p className="titile">Túi Totes Cactus Bag nữ tính xành điệu</p>
+            <div className="price-2">320.000 đ</div>
+            <div className="color-2">
+              <div className="ellipse-2" />
+              <div className="ellipse-3" />
+              <div className="ellipse-4" />
+              <div className="ellipse-5" />
+            </div>
+          </div>
+        </div>
+        <div className="overlap-group-wrapper">
+          <div className="overlap-8">
+            <img className="img-2" alt="Img" src="/img/img-8.svg" />
+            <p className="titile">Túi Totes Cactus Bag nữ tính xành điệu</p>
+            <div className="price-2">320.000 đ</div>
+            <div className="color-2">
+              <div className="ellipse-2" />
+              <div className="ellipse-3" />
+              <div className="ellipse-4" />
+              <div className="ellipse-5" />
+            </div>
+          </div>
+        </div>
+        <div className="overlap-9">
+          <img className="img" alt="Intersect" src="/img/intersect-1.svg" />
+          <div className="card-product-hover">
+            <div className="overlap-10">
+              <img className="img-2" alt="Img" src="/img/img-7.svg" />
+              <p className="titile">Túi Totes Cactus Bag nữ tính xành điệu</p>
+              <div className="overlap-3">
+                <div className="overlap-group-2">
+                  <div className="price">320.000 đ</div>
+                  <img className="cursor-pointer" alt="Cursor pointer" src="/img/cursor-pointer-1.svg" />
+                </div>
+                <div className="color">
+                  <div className="ellipse-2" />
+                  <div className="ellipse-3" />
+                  <div className="ellipse-4" />
+                  <div className="ellipse-5" />
+                </div>
+              </div>
+              <button className="btn">
+                <div className="text-wrapper">Mua ngay</div>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="div-wrapper">
+          <div className="overlap-11">
+            <img className="img-2" alt="Img" src="/img/img-6.svg" />
+            <p className="titile">Túi Totes Cactus Bag nữ tính xành điệu</p>
+            <div className="price-2">320.000 đ</div>
+            <div className="color-2">
+              <div className="ellipse-2" />
+              <div className="ellipse-3" />
+              <div className="ellipse-4" />
+              <div className="ellipse-5" />
+            </div>
+          </div>
+        </div>
+        <div className="card-product-default-2">
+          <div className="overlap-12">
+            <img className="img-2" alt="Img" src="/img/img-5.svg" />
+            <p className="titile">Túi Totes Cactus Bag nữ tính xành điệu</p>
+            <div className="price-2">320.000 đ</div>
+            <div className="color-2">
+              <div className="ellipse-2" />
+              <div className="ellipse-3" />
+              <div className="ellipse-4" />
+              <div className="ellipse-5" />
+            </div>
+          </div>
+        </div>
+        <div className="card-product-default-3">
+          <div className="overlap-13">
+            <img className="img-2" alt="Img" src="/img/img-4.svg" />
+            <p className="titile">Túi Totes Cactus Bag nữ tính xành điệu</p>
+            <div className="price-2">320.000 đ</div>
+            <div className="color-2">
+              <div className="ellipse-2" />
+              <div className="ellipse-3" />
+              <div className="ellipse-4" />
+              <div className="ellipse-5" />
+            </div>
+          </div>
+        </div>
+        <div className="group-2">
+          <p className="text-wrapper-7">Đánh giá của các nàng</p>
+          <p className="p">
+            Mấy đánh giá này là lấy từ facebook qua nè, không có tự bịa ra đâu nha. Hông tin lên facebook mà xem.
+          </p>
+        </div>
+        <div className="group-3">
+          <div className="text-wrapper-7">Tote bán chạy</div>
+          <p className="p">
+            Ra bao nhiêu hàng cũng bán không kịp với mấy cô mà, mà cứ như vậy là Bee thích à nha, hehe
+          </p>
+        </div>
+        <div className="overlap-14">
+          <img className="img" alt="Intersect" src="/img/intersect-1.svg" />
+          <div className="card-product-hover">
+            <div className="overlap-15">
+              <img className="img-2" alt="Img" src="/img/img-3.svg" />
+              <p className="titile">Túi Totes Cactus Bag nữ tính xành điệu</p>
+              <div className="overlap-3">
+                <div className="overlap-group-2">
+                  <div className="price">320.000 đ</div>
+                  <img className="cursor-pointer" alt="Cursor pointer" src="/img/cursor-pointer.svg" />
+                </div>
+                <div className="color">
+                  <div className="ellipse-2" />
+                  <div className="ellipse-3" />
+                  <div className="ellipse-4" />
+                  <div className="ellipse-5" />
+                </div>
+              </div>
+              <button className="btn">
+                <div className="text-wrapper">Mua ngay</div>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card-product-default-4">
+          <div className="overlap-16">
+            <img className="img-2" alt="Img" src="/img/img-2.svg" />
+            <p className="titile">Túi Totes Cactus Bag nữ tính xành điệu</p>
+            <div className="price-2">320.000 đ</div>
+            <div className="color-2">
+              <div className="ellipse-2" />
+              <div className="ellipse-3" />
+              <div className="ellipse-4" />
+              <div className="ellipse-5" />
+            </div>
+          </div>
+        </div>
+        <div className="card-product-default-5">
+          <div className="overlap-17">
+            <img className="img-2" alt="Img" src="/img/img-1.svg" />
+            <p className="titile">Túi Totes Cactus Bag nữ tính xành điệu</p>
+            <div className="price-2">320.000 đ</div>
+            <div className="color-2">
+              <div className="ellipse-2" />
+              <div className="ellipse-3" />
+              <div className="ellipse-4" />
+              <div className="ellipse-5" />
+            </div>
+          </div>
+        </div>
+        <div className="card-product-default-6">
+          <div className="overlap-18">
+            <img className="img-2" alt="Img" src="/img/img.svg" />
+            <p className="titile">Túi Totes Cactus Bag nữ tính xành điệu</p>
+            <div className="price-2">320.000 đ</div>
+            <div className="color-2">
+              <div className="ellipse-2" />
+              <div className="ellipse-3" />
+              <div className="ellipse-4" />
+              <div className="ellipse-5" />
+            </div>
+          </div>
+        </div>
+        <div className="group-4">
+          <div className="text-wrapper-7">Tote nổi bật</div>
+          <p className="p">
+            Ra bao nhiêu hàng cũng bán không kịp với mấy cô mà, mà cứ như vậy là Bee thích à nha, hehe
+          </p>
+        </div>
+        <div className="group-5">
+          <div className="text-wrapper-7">Tote giá “nóng”</div>
+          <p className="p">Đống này đang giảm giá nên rất &#34;nóng”. Bạn còn chờ gì nữa mà &#34;phỏng tay” ngay đi</p>
+        </div>
+        <p className="text-wrapper-8">Siêu phẩm Tote mới ra lò</p>
+        <p className="m-i-ch-ra-c-i-tote-m">
+          Mới &#34;chế&#34; ra cái Tote mới các bác ạ. Thông tin mô tả sơ bộ các bác đọc ở dưới nhé. em đi đóng hàng
+          giao cho khách phát
+        </p>
+        <div className="overlap-19">
+          <div className="flexcontainer">
+            <p className="text">
+              <span className="span">
+                Với cộng đồng nhà thiết kế, chúng tôi là ngôi nhà thời trang,nơi chắp cánh cho hàng vạn mẫu thiết kế của
+                các Designer trên khắp Vietnam.
+                <br />
+              </span>
+            </p>
+            <p className="text">
+              <span className="span">
+                Với khách hàng, BeeYou là nơi bạn dễ dàng tìm thấy những những sản phẩm thời trang thể hiện CHẤT của
+                riêng mình. Là chính mình cùng BeeYou.
+                <br />
+              </span>
+            </p>
+            <p className="text">
+              <span className="span">
+                Với đội ngũ lâu năm kinh nghiệm, có tâm với nghề, những dòng sản phẩm của BeeYou được tuyển chọn những
+                nguyên vật liệu cao cấp nhất, gia công tinh xảo, công nghệ tối tân nhất hiện nay, được kiểm tra chất
+                lượng đầu ra khắt khe.
+                <br />
+              </span>
+            </p>
+            <p className="text">
+              <span className="span">{""}</span>
+            </p>
+          </div>
+          <div className="group-6">
+            <p className="text-wrapper-9">Tote của BeeYou cam kết mang đến sự hài lòng tối đa cho bạn</p>
+          </div>
+          <div className="group-7">
+            <div className="overlap-20">
+              <img className="frame" alt="Frame" src="/img/frame.svg" />
+              <img className="vector" alt="Vector" src="/img/vector-1.svg" />
+              <img className="group-8" alt="Group" src="/img/group-40.png" />
+            </div>
+          </div>
+        </div>
+        <div className="overlap-21">
+          <button className="button">
+            <div className="text-wrapper">Tìm thiểu thêm</div>
+          </button>
+          <p className="text-wrapper-10">Thông tin mô tả sơ bộ</p>
+          <p className="text-wrapper-11">Chất vải canvas mềm mịn, không xù, không nhão</p>
+          <p className="text-wrapper-12">Bee có 5 màu cho bạn lựa canh: Xanh dương, Hồng baby, Vàng chanh,...</p>
+          <div className="text-wrapper-13">Size túi: 38*34*7cm</div>
+          <p className="text-wrapper-14">Túi có khoá kéo, túi con tiện lợi cho nàng sử dụng</p>
+          <div className="tape" />
+          <img className="zipper" alt="Zipper" src="/img/zipper-1.svg" />
+          <img className="textile" alt="Textile" src="/img/textile-1.svg" />
+          <img className="colors" alt="Colors" src="/img/colors-1.svg" />
+          <img className="fit-vertical" alt="Fit vertical" src="/img/fit-vertical-1.svg" />
+        </div>
+        <div className="carrd-instagram">
+          <img className="rectangle-3" alt="Rectangle" src="/img/rectangle-10-5.svg" />
+        </div>
+        <div className="rectangle-wrapper">
+          <img className="rectangle-4" alt="Rectangle" src="/img/rectangle-10-4.svg" />
+        </div>
+        <div className="img-wrapper">
+          <img className="rectangle-3" alt="Rectangle" src="/img/rectangle-10-3.svg" />
+        </div>
+        <div className="carrd-instagram-2">
+          <img className="rectangle-3" alt="Rectangle" src="/img/rectangle-10-2.svg" />
+        </div>
+        <div className="carrd-instagram-3">
+          <img className="rectangle-3" alt="Rectangle" src="/img/rectangle-10-1.svg" />
+        </div>
+        <div className="carrd-instagram-4">
+          <img className="rectangle-5" alt="Rectangle" src="/img/rectangle-10.svg" />
+        </div>
+        <div className="group-9">
+          <div className="text-wrapper-15">BeeYou trên Instagram</div>
+          <p className="text-wrapper-16">
+            Follow BeeYou trên instagram để có cơ hội tham gia các chương trình ưu đãi dành riêng cho người dùng
+            Instagram
+          </p>
+        </div>
+        <div className="group-10">
+          <div className="text-wrapper-17">instagr.am/beeyoufashion</div>
+          <img className="logo-instagram" alt="Logo instagram" src="/img/logo-instagram-1.svg" />
+        </div>
+        <div className="group-11">
+          <div className="overlap-22">
+            <img className="youtube" alt="Youtube" src="/img/youtube-1.svg" />
+            <div className="text-wrapper-18">Sản phẩm hot</div>
+            <div className="text-wrapper-19">Sản phẩm bán chạy</div>
+            <div className="text-wrapper-20">Hot deal tháng 12</div>
+            <div className="text-wrapper-21">Anotner links</div>
+            <div className="text-wrapper-22">© 2020 beebag.com.vn</div>
+            <img className="instagram" alt="Instagram" src="/img/instagram-1.svg" />
+            <img className="facebook" alt="Facebook" src="/img/facebook-1.svg" />
+          </div>
+        </div>
+        <div className="overlap-23">
+          <img className="image-2" alt="Atribute" src="/img/image-16.png" />
+          <img className="screen-shot" alt="Screen shot" src="/img/screen-shot-2019-12-09-at-19-58-1.png" />
+        </div>
+        <img className="screen-shot-2" alt="Screen shot" src="/img/screen-shot-2019-12-09-at-19-58.png" />
+        <div className="group-12">
+          <div className="down-arrow-wrapper">
+            <img className="img-3" alt="Down arrow" src="/img/down-arrow-1-1.svg" />
+          </div>
+          <div className="group-13">
+            <img className="img-3" alt="Down arrow" src="../static/img/down-arrow-1.svg" />
+          </div>
+        </div>
+        <img className="image-3" alt="Atribute" src="/static/img/image-18.png" />
+      </div>
+    </div>
+  );
+};
